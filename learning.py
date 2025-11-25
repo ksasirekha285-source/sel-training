4c1d38cae91e637367e7563bc8d73205a659dda4
ksasirekha285-source <ksasirekha285@gmail.com>
2025-11-25 21:18:33 +0530

    added for loop for primenumber

--- learning.py
+++ learning.py
@@ -5,7 +5,8 @@ class Numbers:
                  print("even")
               else:
                  print("odd")
-                 
+         for i in range(2, 50):
+             for j in range(2, i):
                  if i%j==0:
                      break
                  else:
