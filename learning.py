3d5fe420d3ca94f24a8a4fcacb7b2a86a9cc65b4
ksasirekha285-source <ksasirekha285@gmail.com>
2025-11-25 20:55:08 +0530

    adding function

--- learning.py
+++ learning.py
@@ -1,5 +1,8 @@
-for i in range(1,10):
-if 10%2==0:
-    print("even")
-else:
-    print("odd")
+def even_odd(start,end):
+    for i in range(1,10):
+        if 10%2==0:
+            print("even")
+        else:
+            print("odd")
+
+even_odd(10,20)
